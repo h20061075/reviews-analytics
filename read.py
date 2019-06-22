0881813e7396eee47df78ce13488975b5b50a850
Jean Huang <jeanettecyhuang@gmail.com>
2019-06-22 16:50:13 +0800

    calc avg

--- read.py
+++ read.py
@@ -2,5 +2,8 @@ data = []
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
-print(data[2])
+print('檔案讀取完了,總共有',len(data),'筆資料')
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均留言是',sum_len/len(data))
